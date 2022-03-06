f073d3c742eab1c880c9129ebfcb46b12c437834
Caleb Jasik <calebjasik@jasik.xyz>
2022-03-06 10:49:12 -0600

    Try to escape jsfuzz OOM by moving require into function; it failed

--- fuzz-prettier-plugin-go-template.js
+++ fuzz-prettier-plugin-go-template.js
@@ -1,10 +1,8 @@
-const prettier = require("prettier");
-const GoTemplatePlugin = require("prettier-plugin-go-template");
-
-module.exports = function fuzz(buf) {
+module.exports.fuzz = function fuzz(buf) {
+  const prettier = require("prettier");
+  const GoTemplatePlugin = require("prettier-plugin-go-template");
   try {
     const input = String.fromCodePoint(...buf);
-    console.debug(GoTemplatePlugin);
 
     prettier.format(input, {
       parser: "go-template",
